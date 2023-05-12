e50b22d0e42642516a2689a92cc9e9ebfc3cb983
korisnik <stefana.miladinovic@elfak.rs>
2023-05-12 12:28:55 +0200

    Add project files.

---
+++ SPProjekat1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text.Json.Serialization;
+
+namespace SPProjekat1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Thread serverThread = new Thread(() =>
+            {   
+                Server server = new Server();
+                server.Start();
+            });
+            serverThread.Start();
+        }
+    }
+}
